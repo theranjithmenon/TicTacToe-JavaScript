e52ce7b38391e1625df0054fc7cf25b28609ff3e
Ranjith Menon <60739917+theranjithmenon@users.noreply.github.com>
2022-12-20 10:32:22 +0530

    commit

--- script.js
+++ script.js
@@ -36,32 +36,31 @@ function Game(num) {
                 tile.value = "O";
             }
             whichPlayer = !whichPlayer;
-
-
         }
-        if (one == two && one == three && one != "") {
-            Over(one);
-        } else if (four == five && four == six && four != "") {
-            Over(four);
-        } else if (seven == eight && seven == nine && seven != "") {
-            Over(seven);
-        } else if (one == four && one == seven && one != "") {
-            Over(one);
-        } else if (two == five && two == eight && two != "") {
-            Over(two);
-        } else if (three == six && three == nine && three != "") {
-            Over(three);
-        } else if (one == five && one == nine && one != "") {
-            Over(one);
-        } else if (three == five && three == seven && three != "") {
-            Over(three);
-        }
+    }
+    if (one == two && one == three && one != "") {
+        Over(one);
+    } else if (four == five && four == six && four != "") {
+        Over(four);
+    } else if (seven == eight && seven == nine && seven != "") {
+        Over(seven);
+    } else if (one == four && one == seven && one != "") {
+        Over(one);
+    } else if (two == five && two == eight && two != "") {
+        Over(two);
+    } else if (three == six && three == nine && three != "") {
+        Over(three);
+    } else if (one == five && one == nine && one != "") {
+        Over(one);
+    } else if (three == five && three == seven && three != "") {
+        Over(three);
     }
 }
 
+
 function Over(won) {
     document.getElementById("turn").innerHTML = won + "\t Won the Game";
-    document.getElementById("resetBtn").style.visibility = "visible"
+    document.getElementById("resetBtn").style.visibility = "visible";
 }
 function reset() {
     window.location.reload();
