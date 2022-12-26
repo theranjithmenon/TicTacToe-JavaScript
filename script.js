afd044381c89e22070703a981d674fd8e2dd94e5
Ranjith Menon <60739917+theranjithmenon@users.noreply.github.com>
2022-12-26 19:01:42 +0530

    commit

--- script.js
+++ script.js
@@ -15,28 +15,28 @@ function Game(num) {
 
     var tile = document.getElementById(num);
 
+    if (count == 9) {
+        document.getElementById("head").innerHTML = "DROW";
+        document.getElementById("turn").style.visibility = "hidden";
+        document.getElementById("resetBtn").style.visibility = "visible"
+    }
+    if (tile.value == "") {
+        if (whichPlayer == false) {
+            document.getElementById("turn").innerHTML = "X's Turn";
+        } else {
+            document.getElementById("turn").innerHTML = "O's Turn";
+        }
+        if (whichPlayer == true) {
+            tile.style.backgroundColor = "blue";
+            tile.value = "X";
+        } else {
+            tile.style.backgroundColor = "red";
+            tile.value = "O";
+        }
+        whichPlayer = !whichPlayer;
+    }
     if (document.getElementById("resetBtn").style.visibility != "visible") {
         count++;
-        if (count == 9) {
-            document.getElementById("head").innerHTML = "DROW";
-            document.getElementById("turn").style.visibility = "hidden";
-            document.getElementById("resetBtn").style.visibility = "visible"
-        }
-        if (tile.value == "") {
-            if (whichPlayer == false) {
-                document.getElementById("turn").innerHTML = "X's Turn";
-            } else {
-                document.getElementById("turn").innerHTML = "O's Turn";
-            }
-            if (whichPlayer == true) {
-                tile.style.backgroundColor = "blue";
-                tile.value = "X";
-            } else {
-                tile.style.backgroundColor = "red";
-                tile.value = "O";
-            }
-            whichPlayer = !whichPlayer;
-        }
     }
     if (one == two && one == three && one != "") {
         Over(one);
